898eb026c4f4e6b52255822e44c92e13cf6538b6
Richardas Kuchinskas <36065987+Hidanio@users.noreply.github.com>
2024-06-21 11:44:16 +0300

    Implementation (#1)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "Cow_str.h"
 int main() {
-    CowString A = CowString("abc");
+/*    CowString A = CowString("abc");
     CowString B = A;
 
 
@@ -10,6 +10,15 @@ int main() {
     B[0] = 'd';
     std::cout << B << '\n';
     std::cout << A << '\n';
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;*/
+
+    CowString cowString("hello");
+    cowString.debug();
+    auto cowS = cowString;
+    cowS[1] = 'x';
+    std::cout << "==========================="<<"\n";
+    StringWrapper str("world");
+    str.debug();
+    str.printData();
     return 0;
 }
