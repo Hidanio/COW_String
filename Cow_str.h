d9a71bbc4a177c3dd59fdac4fb79ec9553df9e49
Richardas Kuchinskas <36065987+Hidanio@users.noreply.github.com>
2024-06-28 13:17:15 +0300

    own ref counter (#2)
    * Own refcounter

--- Cow_str.h
+++ Cow_str.h
@@ -1,125 +1,150 @@
-#include <memory>
-#include <iostream>
-#include <string>
-#include <utility>
+#pragma once
 
-#ifndef COW_STR_COW_STR_H
-#define COW_STR_COW_STR_H
+#include <iostream>
+#include <cstring>
 
 class StringWrapper {
 private:
-    std::string data;
+    char *data;
 
 public:
-    explicit StringWrapper(std::string init) : data(std::move(init)) {
-        std::cout << "StringWrapper constructed" << "\n";
+    explicit StringWrapper(const char *init) : data(new char[strlen(init) + 1]) {
+        strcpy(data, init);
     }
 
-    explicit StringWrapper(std::string_view init) : data(init) {
-        std::cout << "StringWrapper constructed" << "\n";
+    StringWrapper(const StringWrapper &other) : data(new char[strlen(other.data) + 1]) {
+        strcpy(data, other.data);
     }
 
-    explicit StringWrapper(const char *init) : data(init) {
-        std::cout << "StringWrapper constructed" << "\n";
+    ~StringWrapper() {
+        delete[] data;
     }
 
-    StringWrapper(StringWrapper const &other): data(other.data){
-        std::cout << "StringWrapper copied" << "\n";
+    char &operator[](size_t index) {
+        std::cout << "StringWrapper operator[] called" << "\n";
+        return data[index];
     }
 
-    StringWrapper(StringWrapper&& other) noexcept : data(std::move(other.data)){
-        std::cout << "StringWrapper moved" << "\n";
+    const char &operator[](size_t index) const {
+        std::cout << "StringWrapper const operator[] called" << "\n";
+        return data[index];
     }
 
-    StringWrapper& operator=(const StringWrapper &other) {
+    StringWrapper &operator=(const StringWrapper &other) {
         if (this != &other) {
-            data = other.data;
+            delete[] data;
+            data = new char[strlen(other.data) + 1];
+            strcpy(data, other.data);
             std::cout << "StringWrapper copy assigned" << "\n";
         }
         return *this;
     }
 
-    StringWrapper& operator=(StringWrapper &&other) noexcept {
+    StringWrapper &operator=(StringWrapper &&other) noexcept {
         if (this != &other) {
-            data = std::move(other.data);
+            data = other.data;
             std::cout << "StringWrapper move assigned" << "\n";
         }
         return *this;
     }
 
-    char& operator[](size_t index) {
-        std::cout << "StringWrapper operator[] called" << "\n";
-        return data[index];
+    StringWrapper operator+(const StringWrapper &other) {
+        size_t new_length = strlen(data) + strlen(other.data);
+        char *new_data = new char[new_length + 1];
+        strcpy(new_data, data);
+        strcat(new_data, other.data);
+        StringWrapper result(new_data);
+        delete[] new_data;
+        return result;
     }
 
-    const char& operator[](size_t index) const {
-        std::cout << "StringWrapper const operator[] called" << "\n";
-        return data[index];
-    }
-
-    ~StringWrapper(){
-        std::cout << "StringWrapper Destroyed" << "\n";
-
+    StringWrapper substr(size_t start, size_t length) {
+        if (start + length > strlen(data)) length = strlen(data) - start;
+        char *sub = new char[length + 1];
+        strncpy(sub, data + start, length);
+        sub[length] = '\0';
+        StringWrapper result(sub);
+        delete[] sub;
+        return result;
     }
 
     void debug() const {
         std::cout << "Data: " << data << std::endl;
     }
 
-    std::string printData() const {
+    [[nodiscard]] const char *getData() const {
         return data;
     }
 
     friend std::ostream &operator<<(std::ostream &os, const StringWrapper &str) {
-        return os << str.data;
+        os << str.data;
+        return os;
     }
 };
 
 class CowString {
 private:
-    std::shared_ptr<StringWrapper> counter_data_;
+    StringWrapper *data;
+    int *ref_count;
+
+    void detach() {
+        if (*ref_count > 1) {
+            (*ref_count)--;
+            data = new StringWrapper(*data);
+            ref_count = new int(1);
+        }
+    }
 
 public:
-    explicit CowString(std::string_view init = "") : counter_data_(std::make_shared<StringWrapper>(init)) {
-        std::cout << "CowString constructed with data: " << *counter_data_ << "\n";
+    CowString(const char *init = "") : data(new StringWrapper(init)), ref_count(new int(1)) {}
+
+    CowString(const CowString &other) : data(other.data), ref_count(other.ref_count) {
+        ++(*ref_count);
     }
 
-    CowString(const CowString &other) : counter_data_(other.counter_data_) {
-        std::cout << "CowString copied" << "\n";
-    }
-
-    CowString& operator=(const CowString &other) {
+    CowString &operator=(const CowString &other) {
         if (this != &other) {
-            counter_data_ = other.counter_data_;
-            std::cout << "CowString assigned" << "\n";
+            if (--(*ref_count) == 0) {
+                delete data;
+                delete ref_count;
+            }
+            data = other.data;
+            ref_count = other.ref_count;
+            (*ref_count)++;
         }
         return *this;
     }
 
+    ~CowString() {
+        if (--(*ref_count) == 0) {
+            delete data;
+            delete ref_count;
+        }
+    }
+
+    CowString operator+(const CowString &other) {
+        detach();
+        return CowString((*data + *other.data).getData());
+    }
+
+    CowString substr(size_t start, size_t length) {
+        return CowString(data->substr(start, length).getData());
+    }
+
     const char &operator[](size_t index) const {
-        std::cout << "operator[] called" << "\n";
-        return (*counter_data_)[index];
+        return (*data)[index];
     }
 
     char &operator[](size_t index) {
-        std::cout << "Non-const operator[] called" << "\n";
-
-        if (!counter_data_.unique()) {
-            counter_data_ = std::make_shared<StringWrapper>(*counter_data_);
-        }
-        std::cout << "Detached for modification" << "\n";
-
-        return (*counter_data_)[index];
+        detach();
+        return (*data)[index];
     }
 
-    void debug() {
-        std::cout << "Pointers: " << counter_data_.get() << " , Data: " << *counter_data_
-                  << ", RefCounter: " << counter_data_.use_count() << "\n";
+    void debug() const {
+        std::cout << "Data: " << data->getData() << ", RefCount: " << *ref_count << "\n";
     }
 
     friend std::ostream &operator<<(std::ostream &os, const CowString &str) {
-        return os << *str.counter_data_;
+        return os << *str.data;
     }
 };
-
-#endif //COW_STR_COW_STR_H
